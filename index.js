26eb466e0ecb17d0932f4f6cb768e851c7a1c039
zacanger <zac@zacanger.com>
2019-01-11 18:37:46 -0700

    Fix a bug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ffi = require('ffi')
+const { resolve } = require('path')
 
-const wrapperPath = 'build/Release/putenv-wrapper'
+const wrapperPath = resolve(__dirname, '/build/Release/putenv-wrapper')
 
 const putenv = ffi.Library(wrapperPath, {
   wrapper: [ 'int', [ 'string' ] ]
