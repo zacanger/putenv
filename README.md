6919cd17d02fd8a779e1baced72f770a38eb9f5b
zacanger <zac@zacanger.com>
2019-01-11 18:44:46 -0700

    Add readme

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # putenv
 
+`putenv(3)` for Node.
+
+I don't think it works yet.
+
 --------
 
 ## Installation
 
+`npm i putenv`
+
 ## Usage
+
+`require('putenv')('foo', 'bar')`
 
 ## License
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ffi = require('ffi')
-const { resolve } = require('path')
 
-const wrapperPath = resolve(__dirname, '/build/Release/putenv-wrapper')
+// eslint-disable-next-line
+const wrapperPath = __dirname + '/build/Release/putenv-wrapper'
 
 const putenv = ffi.Library(wrapperPath, {
   wrapper: [ 'int', [ 'string' ] ]
